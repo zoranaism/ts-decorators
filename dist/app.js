bf980160d112e3c9ba4ee1abe96889d7d08f2079
zoranaism <lazareviczorana@gmail.com>
2020-07-03 16:59:27 +0200

    Decorator factories

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,35 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logger(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
+}
+function withTemplate(template, hookId) {
+    return function (constructor) {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector("h1").textContent = p.name;
+        }
+    };
+}
+let Person = class Person {
+    constructor() {
+        this.name = "Zox";
+        console.log("Creating person object.");
+    }
+};
+Person = __decorate([
+    withTemplate("<h1>My person object </h1>", "app")
+], Person);
+const pers = new Person();
+console.log(pers);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,42 @@
-// Code goes here!
+// ADDING DECORATOR TO THE CLASS
+// decorator is at the end just an function
+
+//decorator
+// function Logger(constructor: Function) {
+//   // target or constructor
+//   console.log("Logging...");
+//   console.log(constructor);
+// }
+
+//decorator factory
+function Logger(logString: string) {
+  // functions that returns new function
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+function withTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+// @Logger("Logging - person")
+@withTemplate("<h1>My person object </h1>", "app")
+class Person {
+  name = "Zox";
+
+  constructor() {
+    console.log("Creating person object.");
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
